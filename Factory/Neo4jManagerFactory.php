1b54590fe4000e03a7bbed07ba9365471bff2f6e
gpenverne <gpenverne@gmail.com>
2016-10-10 15:33:37 +0200

    EntityManager for bolt

--- Factory/Neo4jManagerFactory.php
+++ Factory/Neo4jManagerFactory.php
@@ -3,6 +3,7 @@
 namespace GensDeConfiance\Neo4jOGMManagerBundle\Factory;
 
 use GraphAware\Neo4j\OGM\EntityManager;
+use GraphAware\Bolt\GraphDatabase;
 
 class Neo4jManagerFactory
 {
@@ -16,8 +17,14 @@ class Neo4jManagerFactory
      */
     public function createEntityManager(string $database_host = 'localhost', string $database_user = 'neo4j', string $database_password = 'neo4j', int $database_port = 7474): EntityManager
     {
-        $protocol = function_exists('bcmod') ? 'bolt' : 'http';
-        $entityManager = EntityManager::create(sprintf($protocol.'://%s:%s@%s:%d', $database_user, $database_password, $database_host, $database_port));
+        if (function_exists('bcmod')) {
+            $neo4jServer = GraphDatabase::driver(sprintf('bolt://%s:%s@%s:%d', $database_user, $database_password, $database_host, $database_port));
+            $entityManager = new EntityManager($neo4jServer);
+        } else {
+            $neo4jServer = sprintf('http://%s:%s@%s:%d', $database_user, $database_password, $database_host, $database_port);
+            $entityManager = EntityManager::create($neo4jServer);
+        }
+
         $entityManager->flush();
 
         return $entityManager;
