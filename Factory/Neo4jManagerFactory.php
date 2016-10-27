0031831792f2c63555392b64ee7be33fb62314f8
Grégoire <gpenverne@users.noreply.github.com>
2016-10-27 11:46:27 +0200

    Add cache_dir parameter (#5)
    * Add cache_dir parameter
    * Use symfony cache folder

--- Factory/Neo4jManagerFactory.php
+++ Factory/Neo4jManagerFactory.php
@@ -20,8 +20,15 @@ class Neo4jManagerFactory
      *
      * @return EntityManager
      */
-    public function createEntityManager(string $host = 'localhost', string $user = 'neo4j', string $password = 'neo4j', int $httpPort = 7474, int $boltPort = BoltDriver::DEFAULT_TCP_PORT, string $protocol = self::BOLT_PROTOCOL): EntityManager
-    {
+    public function createEntityManager(
+        string $host = 'localhost',
+        string $user = 'neo4j',
+        string $password = 'neo4j',
+        int $httpPort = 7474,
+        int $boltPort = BoltDriver::DEFAULT_TCP_PORT,
+        string $protocol = self::BOLT_PROTOCOL,
+        string $cacheDir = null
+    ): EntityManager {
         $dsn = sprintf(
             '%s://%s:%s@%s:%d',
             function_exists('bcmod') && $protocol === self::BOLT_PROTOCOL ? self::BOLT_PROTOCOL : self::HTTP_PROTOCOL,
@@ -31,6 +38,6 @@ class Neo4jManagerFactory
             function_exists('bcmod') && $protocol === self::BOLT_PROTOCOL ? $boltPort : $httpPort
         );
 
-        return EntityManager::create($dsn);
+        return EntityManager::create($dsn, $cacheDir);
     }
 }
